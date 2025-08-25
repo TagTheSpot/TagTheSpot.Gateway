ffa797fb4524e5bf1bd81733a0b9f874cdc7b17c
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-08-25 09:10:12 +0300

    Implement the gateway

---
+++ src/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TagTheSpot.Gateway.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddRoutesConfigurationFiles(
+            this IConfigurationBuilder builder)
+        {
+            builder.AddJsonFile("Configurations/clusters.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/user.routes.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/spot.routes.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/moderation.routes.json", optional: true, reloadOnChange: true);
+
+            return builder;
+        }
+    }
+}
